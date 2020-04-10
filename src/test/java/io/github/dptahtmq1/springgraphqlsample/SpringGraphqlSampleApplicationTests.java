cfe828aa3e0090d0645af544bd163bde0e57b8af
JK <dptahtmq1@gmail.com>
2020-04-10 23:04:13 +0900

    add unit test for graphql api execution  - import `graphql-spring-boot-starter-test`  - get 10 posts  - write post

--- src/test/java/io/github/dptahtmq1/springgraphqlsample/SpringGraphqlSampleApplicationTests.java
+++ src/test/java/io/github/dptahtmq1/springgraphqlsample/SpringGraphqlSampleApplicationTests.java
@@ -1,13 +1,52 @@
 package io.github.dptahtmq1.springgraphqlsample;
 
+import com.graphql.spring.boot.test.GraphQLResponse;
+import com.graphql.spring.boot.test.GraphQLTestTemplate;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringGraphqlSampleApplicationTests {
 
+    @Autowired
+    private GraphQLTestTemplate graphQLTestTemplate;
+
+    @Order(1)
     @Test
-    void contextLoads() {
+    public void testGetPost() throws Exception {
+        // Given
+
+        // When
+        GraphQLResponse response = graphQLTestTemplate.postForResource("graphql/getPost.graphql");
+
+        // Then
+        assertNotNull(response);
+        assertTrue(response.isOk());
+        assertEquals("Post00", response.get("$.data.recentPosts[0].id"));
+    }
+
+    @Order(2)
+    @Test
+    public void testWritePost() throws Exception {
+        // Given
+
+        // When
+        GraphQLResponse response = graphQLTestTemplate.postForResource("graphql/writePost.graphql");
+
+        // Then
+        assertNotNull(response);
+        assertTrue(response.isOk());
+        assertNotNull(response.get("$.data.writePost.id"));
     }
 
 }
